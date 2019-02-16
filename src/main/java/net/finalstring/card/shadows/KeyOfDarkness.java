eae9276a1b60452ab61fcb3259e23da8cf13b165
A. Beeler <abeeler.cs@gmail.com>
2019-02-16 01:33:21 -0500

    Implement Key of Darkness

---
+++ src/main/java/net/finalstring/card/shadows/KeyOfDarkness.java
@@ -0,0 +1,24 @@
+package net.finalstring.card.shadows;
+
+import net.finalstring.Player;
+import net.finalstring.card.Card;
+import net.finalstring.card.House;
+import net.finalstring.effect.node.EffectNode;
+import net.finalstring.effect.player.ForgeKey;
+
+public class KeyOfDarkness extends Card {
+    static final int NORMAL_KEY_COST_MODIFIER = 6;
+    static final int CONDITIONAL_KEY_COST_MODIFIER = 2;
+
+    public KeyOfDarkness() {
+        super(273, House.Shadows);
+    }
+
+    @Override
+    protected void buildPlayEffects(EffectNode.Builder effectBuilder, Player player) {
+        super.buildPlayEffects(effectBuilder, player);
+        effectBuilder.effect(new ForgeKey(player, player.getOpponent().getAemberPool() == 0 ?
+                CONDITIONAL_KEY_COST_MODIFIER :
+                NORMAL_KEY_COST_MODIFIER));
+    }
+}
---
+++ src/test/java/net/finalstring/card/shadows/KeyOfDarknessTest.java
@@ -0,0 +1,38 @@
+package net.finalstring.card.shadows;
+
+import net.finalstring.Player;
+import net.finalstring.card.AbstractCardTest;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class KeyOfDarknessTest extends AbstractCardTest<KeyOfDarkness> {
+    @Test public void testPlayingWithoutEnoughAemberDoesNothing() {
+        play(underTest);
+
+        assertThat(player.getAemberPool(), is(STARTING_AEMBER));
+        assertThat(player.getForgedKeys(), is(0));
+    }
+
+    @Test public void testForgingExpensiveKey() {
+        int expensiveKeyCost = Player.DEFAULT_KEY_COST + KeyOfDarkness.NORMAL_KEY_COST_MODIFIER;
+        player.addAember(expensiveKeyCost - STARTING_AEMBER);
+
+        play(underTest);
+
+        assertThat(player.getAemberPool(), is(0));
+        assertThat(player.getForgedKeys(), is(1));
+    }
+
+    @Test public void testForgingSlightlyMoreExpensiveKey() {
+        int expensiveKeyCost = Player.DEFAULT_KEY_COST + KeyOfDarkness.CONDITIONAL_KEY_COST_MODIFIER;
+        player.addAember(expensiveKeyCost - STARTING_AEMBER);
+        opponent.setAember(0);
+
+        play(underTest);
+
+        assertThat(player.getAemberPool(), is(0));
+        assertThat(player.getForgedKeys(), is(1));
+    }
+}
